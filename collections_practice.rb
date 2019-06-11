cbff5a744b6fcbe31b45c975a16fe724911beef1
mariah <mariah@gardziola.com>
2019-06-11 03:00:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[0], array[3] = array[3], array[0]
+  array[1], array[2] = array[2], array[1]
 end
 
 def reverse_array(array)
