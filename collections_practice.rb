fff80b6d59f3d6f577cd107898765f41e50e0fe8
mariah <mariah@gardziola.com>
2019-06-11 03:23:42 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,12 +3,42 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
+  array.sort.reverse
 end
 
 def sort_array_char_count(array)
+  array.sort_by {|arr| arr.length}
 end
 
 def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+ array.each_with_index {|word, index| word[2] = "$"}
+ array
+end
+
+def find_a(array)
+  array.select{|element| element.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0){|sum, x| sum + x}
+end
+
+def add_s(array)
+  array.each_with_index.map do |element,index|
+    if index == 1
+      element
+    else
+      element << "s"
+    end
+  end
+end
 
